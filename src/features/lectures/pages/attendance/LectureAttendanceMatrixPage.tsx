514bccff6c34aaa8ddc51fc4c30ef366a0c03664
hyunjin <guswls3028@naver.com>
2026-02-14 07:18:18 +0900

    sync local with repo (auto)

--- src/features/lectures/pages/attendance/LectureAttendanceMatrixPage.tsx
+++ src/features/lectures/pages/attendance/LectureAttendanceMatrixPage.tsx
@@ -73,7 +73,7 @@ export default function LectureAttendanceMatrixPage() {
         <div style={{ width: "fit-content" }}>
           <DomainTable
             tableClassName="ds-table--flat ds-table--attendance"
-            tableStyle={{ minWidth: tableMinWidth, width: "100%", tableLayout: "fixed" }}
+            tableStyle={{ width: tableMinWidth, minWidth: tableMinWidth, tableLayout: "fixed" }}
           >
             <colgroup>
               <col style={{ width: col.checkbox }} />
