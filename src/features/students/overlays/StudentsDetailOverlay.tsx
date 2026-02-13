bd0decf208612b69b512534268b077544c59607e
hyunjin <guswls3028@naver.com>
2026-02-14 07:10:17 +0900

    sync local with repo (auto)

--- src/features/students/overlays/StudentsDetailOverlay.tsx
+++ src/features/students/overlays/StudentsDetailOverlay.tsx
@@ -18,6 +18,7 @@ import {
 
 import StudentFormModal from "../components/EditStudentModal";
 import TagCreateModal from "../components/TagCreateModal";
+import StudentNameWithLectureChip from "@/shared/ui/chips/StudentNameWithLectureChip";
 import { EmptyState, Button } from "@/shared/ui/ds";
 import { formatPhone, formatStudentPhoneDisplay, formatOmrCode } from "@/shared/utils/formatPhone";
 import { STATUS_ACTIVE_COLOR, STATUS_INACTIVE_COLOR } from "@/shared/ui/domain";
