1f60228ffacc3a182c79fc6573d27fed18799bfc
hyunjin <guswls3028@naver.com>
2026-02-17 10:32:15 +0900

    sync local with repo (auto)

--- src/features/staff/StaffRoutes.tsx
+++ src/features/staff/StaffRoutes.tsx
@@ -1,9 +1,12 @@
-﻿import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import StaffLayout from "./StaffLayout";
 
 import HomePage from "./pages/HomePage/HomePage";
-import OperationsPage from "./pages/OperationsPage/OperationsPage";
+import AttendancePage from "./pages/AttendancePage/AttendancePage";
+import ExpensesPage from "./pages/ExpensesPage/ExpensesPage";
+import MonthLockPage from "./pages/MonthLockPage/MonthLockPage";
+import PayrollSnapshotPage from "./pages/PayrollSnapshotPage/PayrollSnapshotPage";
 import ReportsPage from "./pages/ReportsPage/ReportsPage";
 import StaffDetailOverlay from "./overlays/StaffDetailOverlay/StaffDetailOverlay";
 
@@ -13,7 +16,10 @@ export default function StaffRoutes() {
       <Route element={<StaffLayout />}>
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<HomePage />} />
-        <Route path="operations" element={<OperationsPage />} />
+        <Route path="attendance" element={<AttendancePage />} />
+        <Route path="expenses" element={<ExpensesPage />} />
+        <Route path="month-lock" element={<MonthLockPage />} />
+        <Route path="payroll-snapshot" element={<PayrollSnapshotPage />} />
         <Route path="reports" element={<ReportsPage />} />
       </Route>
 
