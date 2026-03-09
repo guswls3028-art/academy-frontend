92cf2e01c58d541a8f78928e6370bbc575806266
hyunjin <guswls3028@naver.com>
2026-03-09 19:06:31 +0900

    sync local with repo (auto)

--- src/features/exams/components/AnswerKeyRegisterModal.tsx
+++ src/features/exams/components/AnswerKeyRegisterModal.tsx
@@ -182,7 +182,7 @@ export default function AnswerKeyRegisterModal({
   const hasQuestions = questions.length > 0;
 
   return (
-    <AdminModal open onClose={onClose} type="action" width={MODAL_WIDTH.xwide}>
+    <AdminModal open onClose={onClose} type="action" width={MODAL_WIDTH.answerKey}>
       <ModalHeader
         type="action"
         title={
